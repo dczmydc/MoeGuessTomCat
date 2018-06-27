2f590e919645670bcf346786115f38253b967b8c
Mingyuan Zhong <zhongm1@cs.uchicago.edu>
2018-06-26 22:58:32 -0700

    read and split answers

--- src/com/example/TestServlet.java
+++ src/com/example/TestServlet.java
@@ -1,6 +1,7 @@
 package com.example;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -26,11 +27,34 @@ public class TestServlet extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
-		
 		PrintWriter out = response.getWriter();
-		out.println("<h1>" + message + "</h1>");
+		out.println("<h1> GET " + message + "</h1>");
 	}
-	
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1> POST " + message + "</h1>");
+		System.out.println("REQ LEN " + request.getContentLength());
+		// Read from request
+		StringBuilder buffer = new StringBuilder();
+		BufferedReader reader = request.getReader();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			buffer.append(line);
+		}
+		String data = buffer.toString();
+		data = data.replaceAll("%25", "%");
+		System.out.println("YOUR ANSWER:");
+		String [] answers = data.split("&");
+		for(int i = 0; i < answers.length; i++) {
+			String[] contents = answers[i].split("=");
+			System.out.println(contents[0] + ": " + contents[1]);
+	    }
+	    //call func to save answer
+	}
+
 	public void destroy() {}
 	
 }
